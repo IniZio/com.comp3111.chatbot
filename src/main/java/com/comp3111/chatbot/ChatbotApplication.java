fb07174c0c92c7881d8479649650f42eac36aa72
Patrick J. Wu <wotingwu@live.com>
2017-10-03 12:20:58 +0800

    Callback For First Testing

--- src/main/java/com/comp3111/chatbot/ChatbotApplication.java
+++ src/main/java/com/comp3111/chatbot/ChatbotApplication.java
@@ -1,12 +1,18 @@
 package com.comp3111.chatbot;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatbotApplication {
+	static Path downloadedContentDir;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		downloadedContentDir = Files.createTempDirectory("line-bot");
 		SpringApplication.run(ChatbotApplication.class, args);
 	}
 }
