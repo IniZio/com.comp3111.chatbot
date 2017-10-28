90b1369224d1ac60eae688ea6a759dc2dd5c8117
Alice Deveops <hlmaab@connect.ust.hk>
2017-10-28 19:22:48 +0800

    update Connection

--- src/main/java/com/comp3111/chatbot/URLConnectionReader.java
+++ src/main/java/com/comp3111/chatbot/URLConnectionReader.java
@@ -5,11 +5,26 @@ import java.io.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 
 public class URLConnectionReader {
-    public static void main(String args) throws Exception {
+    void SearchPeople(String args) throws Exception {
     		Document doc = Jsoup.connect("http://www.ust.hk/search/"+args+"/?sopt=people").get();
+    		
+    		Element content = doc.getElementById("p-s-table");
+    	    Elements inputElements = content.getElementsByTag("td"); 
+
+
+    	    for(Element td : inputElements) {
+    	        String value = td.text();  
+    	        //System.out.println("result: "+value);   			
+    		}
+
+    	    
+    		//String [] result=new String [6];
+    		//return new people (result[0], result[1], result[2], result[3], result[4], result[5]);
     }
 }
--- src/main/java/com/comp3111/chatbot/people.java
+++ src/main/java/com/comp3111/chatbot/people.java
@@ -5,20 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class people{
+	private String title;
 	private String name;
 	private String email;
 	private String phone;
 	private String department;
 	private String room;
 	
-	public people( String name,  String email,  String phone,
+	public people( String title, String name,  String email,  String phone,
 			 String department,  String room){
+		this.title=title;
 		this.name=name;
 		this.name=email;
 		this.name=phone;
 		this.name=department;
 		this.name=room;
 		
+	}
+	
+	public String getTitle() {
+		return this.title;
 	}
 	
 	public String getName() {
