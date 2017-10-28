dd4624181d1b4eec8c2fcc6cdb7d4d5ee276d08d
Alice Deveops <hlmaab@connect.ust.hk>
2017-10-28 16:08:37 +0800

    import Jsoup

--- src/main/java/com/comp3111/chatbot/URLConnectionReader.java
+++ src/main/java/com/comp3111/chatbot/URLConnectionReader.java
@@ -3,6 +3,11 @@ package com.comp3111.chatbot;
 import java.net.*;
 import java.io.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+
 public class URLConnectionReader {
     public static void main(String args) throws Exception {
     		Document doc = Jsoup.connect("http://www.ust.hk/search/"+args+"/?sopt=people").get();
